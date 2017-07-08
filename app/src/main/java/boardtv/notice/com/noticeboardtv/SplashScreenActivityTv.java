fb8f7c111f1b26f00f4f4d39e4cb5ec61f6d305d
vinodrmane <vinod10491@gmail.com>
2017-07-08 18:48:39 +0530

    Initial commit

---
+++ app/src/main/java/boardtv/notice/com/noticeboardtv/SplashScreenActivityTv.java
@@ -0,0 +1,50 @@
+package boardtv.notice.com.noticeboardtv;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+
+/**
+ * Created by Vinod on 6/29/2017.
+ */
+public class SplashScreenActivityTv extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_layout_tv);
+
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        final boolean ranBefore = preferences.getBoolean("RanBefore", false);
+        if (!ranBefore) {
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("RanBefore", true);
+            editor.commit();
+
+        }
+        if (ranBefore==true){
+            Log.v("SPLASH","true");
+            startActivity(new Intent(SplashScreenActivityTv.this, SplashScreenActivityTv.class));
+        }else{
+            Log.v("SPLASH","false");
+            Thread screenDisplayTimer = new Thread(){
+                public void run(){
+                    try {
+                        sleep(1000);
+                    } catch (InterruptedException e) {
+                    }finally{
+                        startActivity(new Intent(SplashScreenActivityTv.this, SplashScreenActivityTv.class));
+                        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                    }
+                    finish();
+                }
+            };
+            screenDisplayTimer.start();
+        }
+
+
+    }
+}
